a57acd8417cf0c17f0b4695c7e3cd4f9bc3b323d
xiaozhou <332506119@qq.com>
2025-03-11 17:32:26 +0800

    ooo

--- src/handler.py
+++ src/handler.py
@@ -14,7 +14,7 @@ cog_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 # ----------------------------- Start API Service ---------------------------- #
 # Call "python -m cog.server.http" in a subprocess to start the API service.
-subprocess.Popen(["python", "-m", "cog.server.http"])
+subprocess.Popen(["python", "-m", "cog.server.http", "--upload-url", "https://upload.10xi.top/upload"])
 
 
 # ---------------------------------------------------------------------------- #
